0f4c9ced82ec93e303901dc696022718e50dc406
sondaydungso <82167090+sondaydungso@users.noreply.github.com>
2023-10-24 02:45:27 +1100

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 // Database connection code here
 require ("settings.php");
 
@@ -10,7 +10,9 @@ $password = $_POST['password'];
 
 // Retrieve user's data from the database
 $sql = "SELECT * FROM managers WHERE username = '$username'";
+$check_lock_sql = "SELECT account_locked FROM managers WHERE username = '$username'";
 $result = $conn->query($sql);
+$result1 = $conn->query($check_lock_sql);
 
 if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
@@ -19,8 +21,8 @@ if ($result->num_rows == 1) {
     // Check if the password is correct
     if ($password == $hashed_password) {
         // Reset login attempts and update last login timestamp
-        $reset_attempts_sql = "UPDATE managers SET login_attempts = 0, last_login_attempt = NOW() WHERE username = '$username'";
-        $conn->query($reset_attempts_sql);
+        $reset_attempts = "UPDATE managers SET login_attempts = 0, last_login_attempt = NOW() WHERE username = '$username'";
+        $conn->query($reset_attempts);
 
         // Store manager's ID in the session for access control
         $_SESSION['manager_id'] = $row['id'];
@@ -29,20 +31,34 @@ if ($result->num_rows == 1) {
         exit;
     } else {
         // Password is incorrect
-        $update_attempts_sql = "UPDATE managers SET login_attempts = login_attempts + 1, last_login_attempt = NOW() WHERE username = '$username'";
-        $conn->query($update_attempts_sql);
+        $update_attempts = "UPDATE managers SET login_attempts = login_attempts + 1, last_login_attempt = NOW() WHERE username = '$username'";
+        $conn->query($update_attempts);
 
         $attempts = $row['login_attempts'] + 1;
-        if ($attempts >= 3) {
-            // Lock the account if there are 3 or more failed login attempts
-            // You can implement account lockout logic here
-            // For example, set a flag or disable the account temporarily
+        if ($attempts >= 6) {
+            $lock_account = "UPDATE managers SET account_locked = 1 WHERE username = '$username'";
+            $conn->query($lock_account);
+            echo "Your account has been locked. Please contact the manager team."; 
         }
 
         echo "Invalid password. Try again.";
     }
 } else {
+    $update_attempts = "UPDATE managers SET login_attempts = login_attempts + 1, last_login_attempt = NOW() WHERE username = '$username'";
+    $conn->query($update_attempts);
     echo "Username not found.";
+}
+
+
+
+
+if ($result1->num_rows == 1) {
+    $row = $result1->fetch_assoc();
+    if ($row["account_locked"] == 1) {
+        // Account is locked
+        echo "Your account is locked. Please contact support for assistance.";
+        exit;
+    }
 }
 
 $conn->close();
